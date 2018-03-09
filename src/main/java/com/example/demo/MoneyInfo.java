d05900e0c746e67d768fbed84183a9e4ba4619b5
xiansheng123 <732824600@qq.com>
2018-03-10 00:04:45 +0800

    init

--- src/main/java/com/example/demo/MoneyInfo.java
+++ src/main/java/com/example/demo/MoneyInfo.java
@@ -1,4 +1,11 @@
 package com.example.demo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class MoneyInfo {
+    private String company;
+    private String rate;
 }
--- src/main/java/com/example/demo/controller/SGToCNY.java
+++ src/main/java/com/example/demo/controller/SGToCNY.java
@@ -1,4 +1,22 @@
 package com.example.demo.controller;
 
+import com.example.demo.MoneyInfo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import sun.awt.SunHints;
+
+import java.util.*;
+
+@Controller
+@RequestMapping("/CN")
 public class SGToCNY {
+    @RequestMapping(value = "GetAll")
+    public List<MoneyInfo> getAll() {
+        return  new ArrayList<MoneyInfo> () {
+            {
+                add (new MoneyInfo ("CompanyOfDa", "4.85"));
+                add (new MoneyInfo ("CompanyOfZou", "4.89"));
+            }
+        };
+    }
 }
