d79660f0ec36dc1ebe1b200c0704c9ef37772dbc
xiansheng123 <732824600@qq.com>
2018-03-11 21:59:38 +0800

    add ChangeCheng & ShangLi web site

--- src/main/java/com/example/demo/controller/SGToCNY.java
+++ src/main/java/com/example/demo/controller/SGToCNY.java
@@ -3,21 +3,15 @@ package com.example.demo.controller;
 import com.example.demo.MoneyInfo;
 import com.example.demo.service.ClientHttp;
 import lombok.AllArgsConstructor;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 @RestController
-@RequestMapping("/cn")
+@RequestMapping("/sgdToCn")
 @AllArgsConstructor
 public class SGToCNY {
 
